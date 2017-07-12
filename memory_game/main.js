6dea284cf3e689c9b21fdbf6ff8ed2f41df1c795
Richard Boles <richardboles86@gmail.com>
2017-07-12 08:33:12 -0400

    changes to memory game

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,18 +1,20 @@
 console.log("Up and running!");
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+
 var cardOne = cards[0];
-cardsInPlay.push("queen");
-console.log("User flipped queen");
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
 
 var cardTwo = cards[2];
-cardsInPlay.push("king");
-console.log("User flipped king");
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
 if (cardsInPlay.length === 2) {
     console.log("they match");
   } else if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
-    alert("Sorry, try again");
+    alert("Sorry, try again.");
   }
-
