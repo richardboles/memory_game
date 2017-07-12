f00233453e3cd8d060856a82a4b6021b8de20921
Richard Boles <richardboles86@gmail.com>
2017-07-12 09:15:46 -0400

    added changes by calling a function inside a function

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,20 +1,37 @@
-console.log("Up and running!");
+//console.log("Up and running!");
+
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+}
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cardId);
 
-if (cardsInPlay.length === 2) {
+ if (cardsInPlay.length === 2) {
     console.log("they match");
   } else if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
